501c6ca60d58b9c2eb26689226a3caeada2c3296
Albin Larsson <mail@albinlarsson.com>
2018-07-09 23:50:50 +0200

    Break linting, to verify that reviewdog doesn't show this (only the actual PR change)

--- src/js/utils/loadImage.js
+++ src/js/utils/loadImage.js
@@ -9,9 +9,9 @@ export default function loadImage(src, minWidth = 1) {
         const image = new Image();
 
         const handler = () => {
-            delete image.onload;
-            delete image.onerror;
-            (image.naturalWidth >= minWidth ? resolve : reject)(image);
+          delete image.onload;
+          delete image.onerror;
+          (image.naturalWidth >= minWidth ? resolve : reject)(image);
         };
 
         Object.assign(image, { onload: handler, onerror: handler, src });

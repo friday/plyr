30ab6d6e7350eed60951f763304d031179e22e37
stickler-ci <support@stickler-ci.com>
2018-07-10 18:26:15 +0000

    Fixing style errors.

--- src/js/captions.js
+++ src/js/captions.js
@@ -3,18 +3,18 @@
 // TODO: Create as class
 // ==========================================================================
 
-import controls from "./controls";
+import controls from './controls';
 import i18n from './i18n';
 import support from './support';
 import { dedupe } from './utils/arrays';
 import browser from './utils/browser';
 import {
-  createElement,
-  emptyElement,
+    createElement,
+    emptyElement,
     getAttributesFromSelector,
     insertAfter,
     removeElement,
-    toggleClass
+    toggleClass,
 } from './utils/elements';
 import { on, triggerEvent } from './utils/events';
 import fetch from './utils/fetch';
